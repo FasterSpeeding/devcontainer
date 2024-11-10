b6e6bbb9c059d5383aa8e8dcecf3b9140f3e538f
FasterSpeeding <lucina@lmbyrne.dev>
2024-11-10 15:38:13 +0000

    Update install_vs_server.sh

--- install_vs_server.sh
+++ install_vs_server.sh
@@ -10,8 +10,8 @@ else
     sha=$(curl -s "https://api.github.com/repos/$repo/git/tags/$tag_sha" | jq '.object.sha')
 fi
 
-download_path="~/.vscode-server/cli/servers/Stable-$sha/server" && \
-mkdir $download_path --parents && \
-curl -L https://update.code.visualstudio.com/commit:$sha/server-linux-x64-web/stable | tar xz --strip 1 -C $download_path && \
-mv $download_path/bin/code-server ~/.vscode-server/code-$sha && \
-echo '["$sha"]' >> lru.json && \
+download_path="~/.vscode-server/cli/servers/Stable-$sha/server"
+mkdir $download_path --parents
+curl -L https://update.code.visualstudio.com/commit:$sha/server-linux-x64-web/stable | tar xz --strip 1 -C $download_path
+mv $download_path/bin/code-server ~/.vscode-server/code-$sha
+echo '["$sha"]' >> lru.json
