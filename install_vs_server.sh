f17dceb54951ae2e0e0b93498d8602f53e86e174
FasterSpeeding <lucina@lmbyrne.dev>
2024-11-10 15:52:07 +0000

    Pre-install vscode server (#19)

---
+++ install_vs_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+repo=microsoft/vscode
+
+tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | jq -r '.tag_name')
+read type tag_sha < <(echo $(curl -s "https://api.github.com/repos/$repo/git/ref/tags/$tag" | 
+     jq -r '.object.type,.object.sha'))
+
+if [ $type == "commit" ]; then
+    sha=$tag_sha
+else
+    sha=$(curl -s "https://api.github.com/repos/$repo/git/tags/$tag_sha" | jq '.object.sha')
+fi
+
+download_path="$HOME/.vscode-server/cli/servers/Stable-$sha/server"
+mkdir $download_path --parents
+curl -L https://update.code.visualstudio.com/commit:$sha/server-linux-x64-web/stable | tar xz --strip 1 -C $download_path
+mv $download_path/bin/code-server ~/.vscode-server/code-$sha
+echo '["$sha"]' >> lru.json
