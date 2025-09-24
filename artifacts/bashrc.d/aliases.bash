a302f1595b0cc9227011a6a57616b53f63c5e978
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-24 20:07:43 +0100

    Allow sudo to call user-defined aliases

--- artifacts/bashrc.d/aliases.bash
+++ artifacts/bashrc.d/aliases.bash
@@ -12,3 +12,6 @@ alias l.='eza -d .*'
 alias l1='eza -1'
 alias ll='eza -l --icons=auto --group-directories-first'
 alias ls='eza'
+
+# Sudo alias hack to allow sudo to call user-defined aliases.
+alias sudo='sudo '
