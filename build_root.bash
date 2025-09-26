e5ea7d10276286c847f62c7a6a77b66fd6ad1673
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-26 13:39:14 +0100

    Update build_root.bash

--- build_root.bash
+++ build_root.bash
@@ -11,12 +11,14 @@ dnf distro-sync -y
 dnf copr enable jdxcode/mise -y
 
 dnf install @c-development @development-tools \
-    automake ca-certificates gcc kernel-devel nano openssl \
-    # Install Python build dependencies
-    bzip2 bzip2-devel gdbm-libs libffi-devel libnsl2 libuuid-devel libzstd-devel \
-    llvm readline-devel rsync sqlite-devel tk-devel xz-devel zlib-devel \
-    # Install User tools
-    bash-completion curl iputils lsof man man-db \
+    automake ca-certificates gcc kernel-devel nano openssl -y
+
+# Install Python build dependencies
+dnf install bzip2 bzip2-devel gdbm-libs libffi-devel libnsl2 libuuid-devel \
+    libzstd-devel llvm readline-devel rsync sqlite-devel tk-devel xz-devel zlib-devel -y
+
+# Install User tools
+dnf install bash-completion curl iputils lsof man man-db \
     man-pages mise ps p7zip ugrep wget which zlib -y
 
 # Update man pages
